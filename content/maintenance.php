adab997be10b65adf8c7f5103194900c356bf938
Dorge47 <Dorge47@users.noreply.github.com>
2023-08-07 18:58:04 -0700

    Add public encryption key

--- content/maintenance.php
+++ content/maintenance.php
@@ -1,6 +1,8 @@
-<p>Website's borked, I'll probably fix it</p>
+<h1>Website's borked, I'll probably fix it</h1>
 <br>
-<p>-Chris</p>
+<h2>-Chris</h2>
+<br>
+<a href="Chris_Andrade_0xAE9A1BE8_public.asc">My public encryption key</a>
 </body>
 
 </html>
