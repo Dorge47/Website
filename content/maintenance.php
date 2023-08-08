15a518f2b40582e2f11e50bbd3a2103b52360d38
Dorge47 <Dorge47@users.noreply.github.com>
2023-08-07 19:51:45 -0700

    Change links to raw GitHub file

--- content/maintenance.php
+++ content/maintenance.php
@@ -2,7 +2,7 @@
 <br>
 <h2>-Chris</h2>
 <br>
-<a href="Chris_Andrade_0xAE9A1BE8_public.asc">My public encryption key</a>
+<a href="https://raw.githubusercontent.com/Dorge47/Website/main/Chris_Andrade_0xAE9A1BE8_public.asc" target="_blank">My public encryption key</a>
 </body>
 
 </html>
